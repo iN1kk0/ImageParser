1ffb3fdd3c2e8766069d6cd21b230ec612fd944a
Nikolay <Nikolay17@ukr.net>
2013-07-17 23:19:59 +0300

    Used CURL

--- form-html.php
+++
@@ -1,8 +0,0 @@
-<h2>Парсер изображений</h2>
-Введите URL: <br />
-<form action="/wp-admin/admin.php?page=parser" id="form1" method="post">
-    <input type="text" name="url" >
-    <input type="submit" name="submit" value="Go!">
-</form>
-<br/>
-<b>URL:</b> <?php echo $_POST['url']; ?> <br/>
